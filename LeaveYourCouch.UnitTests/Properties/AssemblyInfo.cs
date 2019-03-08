1ae306eaf767d69c14c6da178bc331ff9d0e81e7
bruno chappe <bruno.chappe@gmail.com>
2019-03-08 10:25:58 +0100

    ajout Test unitaire

---
+++ LeaveYourCouch.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeaveYourCouch.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeaveYourCouch.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ae48aa2-ae73-4681-bba0-7106a4378c6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeaveYourCouch.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using LeaveYourCouch.Api.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeaveYourCouch.UnitTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void TestGoogleApiCitySearchSequence()
+        {
+
+            HomeController hc = new HomeController();
+
+            var result = hc.SearchCity("Lyon").Result;
+
+            Assert.IsInstanceOfType(result, typeof(JsonResult));
+            JsonResult routeResult = result as JsonResult;
+            
+            Assert.IsTrue(routeResult.Data.ToString().Contains("Villeurbanne"));
+            Assert.IsTrue(routeResult.Data.ToString().Contains("Tassin"));
+        }
+    }
+}
